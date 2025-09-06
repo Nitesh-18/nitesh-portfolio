20299b4618710cc93f89fa1db7185cf958e383a9
Nitesh <niteshranjankar5@gmail.com>
2025-09-07 00:44:53 +0530

    Refactor next-sitemap configuration to use CommonJS module exports for improved compatibility

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,19 @@
-export const siteUrl = "https://nitesh-portfolio-three.vercel.app";
-export const generateRobotsTxt = true;
-export const changefreq = "weekly";
-export const priority = 0.7;
-export const sitemapSize = 7000;
-export const exclude = ["/server-sitemap.xml"];
-export const alternateRefs = [
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://nitesh-portfolio-three.vercel.app",
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.7,
+  sitemapSize: 7000,
+  exclude: ["/server-sitemap.xml"],
+  alternateRefs: [
     {
-        href: "https://nitesh-portfolio-three.vercel.app",
-        hreflang: "en",
+      href: "https://nitesh-portfolio-three.vercel.app",
+      hreflang: "en",
     },
     {
-        href: "https://nitesh-portfolio-three.vercel.app",
-        hreflang: "x-default",
+      href: "https://nitesh-portfolio-three.vercel.app",
+      hreflang: "x-default",
     },
-];
+  ],
+};
